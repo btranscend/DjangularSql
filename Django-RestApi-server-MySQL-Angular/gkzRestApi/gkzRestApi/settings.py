0fce65f3da8b0bf02266cd96c8e86fc6353808ff
brianktran <bkt5031@gmail.com>
2019-06-20 16:04:02 -0400

    djangular! its alive updtd

--- Django-RestApi-server-MySQL-Angular/gkzRestApi/gkzRestApi/settings.py
+++ Django-RestApi-server-MySQL-Angular/gkzRestApi/gkzRestApi/settings.py
@@ -57,9 +57,10 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = ['http://localhost:4200',
-'http://localhost:8000',
-'http://localhost:8080',
+CORS_ORIGIN_WHITELIST = [
+'http://localhost:4200',
+# 'http://localhost:8000',
+# 'http://localhost:8080',
 ]
 # (
 
